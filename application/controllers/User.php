115fab5a7219be69ca4986c25e9f1414aade3263
Nicodemus Karisa <nkarisa@ke.ci.org>
2021-06-07 00:01:26 +0300

    First Commit

---
+++ application/controllers/User.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class User extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+    public function index()
+    {
+    }
+
+    function login()
+    {
+        $post = $this->input->post();
+
+        $result = $this->db->get_where(
+            'user',
+            array('user_email' => 'nkmwambs@gmail.com', 'user_password' => '@Compassion123')
+        );
+
+        if ($result->num_rows() > 0) {
+            return json_encode($result->result_array());
+        } else {
+            return json_encode(['msg' => "Invalid Email or Password"]);
+        }
+    }
+
+    function register()
+    {
+    }
+}
