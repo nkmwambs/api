1588621c4f83704948cc19a510fa2548dcf51860
Nicodemus Karisa <nkarisa@ke.ci.org>
2021-07-29 11:33:31 +0300

    Added more select fields for goals

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -25,7 +25,7 @@ class User extends CI_Controller
 
     function goals()
     {
-        $this->db->select(array('goal_id', 'goal_name', 'theme_name'));
+        $this->db->select(array('goal_id', 'goal_name', 'theme_name', 'goal_start_date', 'goal_end_date'));
         //$this->db->where(array("theme_status" => 1));
         $this->db->join('theme', 'theme.theme_id=goal.theme_id');
         $goals["data"] = $this->db->get("goal")->result_array();
