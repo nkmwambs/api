7d02792b2575f90b6913058320323626fe209398
Nicodemus Karisa <NKarisa@us.ci.org>
2021-11-26 16:36:15 +0300

    Adding a language phrase functionality - 7

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -772,7 +772,7 @@ class User extends CI_Controller
 
         
         $this->db->select(array('user_id','user_name','user_first_name','user_last_name','user_email','user_age',
-        'user_address','profile_name','country_name','language_name'));
+        'user_address','profile_name','country_name','language_name','language_code'));
 
         $this->db->join('profile','profile.profile_id=user.profile_id');
         $this->db->join('country','country.country_id=user.country_id');
