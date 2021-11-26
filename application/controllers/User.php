862ce1a003324bca9fa570e11cf6672c92d272b7
Nicodemus Karisa <NKarisa@us.ci.org>
2021-11-26 11:41:19 +0300

    Working on dates computation

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -4,13 +4,61 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class User extends CI_Controller
 {
 
+    private $fy_year_digits = 2;
+    private $fy_start_month = 7;
+    private $fy_year_reference = 'next';
+
     function __construct()
     {
         parent::__construct();
         $this->load->database();
+
+        $this->set_settings();
+
     }
+
     public function index()
     {
+    }
+
+    private function set_settings(){
+        $settings = $this->settings();
+
+        extract($settings);
+
+        $this->fy_year_digits = isset($fy_year_digits) ? $fy_year_digits : $this->create_setting('fy_year_digits',$this->fy_year_digits,'Possible values are 2 or 4');
+        $this->fy_start_month = isset($fy_start_month) ? $fy_start_month : $this->create_setting('fy_start_month', $this->fy_start_month, 'Possible values are 1 to 12');
+        $this->fy_year_reference = isset($fy_year_reference) ? $fy_year_reference : $this->create_setting('fy_year_reference', $this->fy_year_reference, 'Possible values are next or previous');
+    }
+
+    private function create_setting($setting_name, $setting_value, $setting_description){
+        $data['setting_name'] = $setting_name;
+        $data['setting_value'] = $setting_value;
+        $data['setting_description'] = $setting_description;
+        $data['setting_created_by'] = 1;
+        $data['setting_created_date'] = date('Y-m-d');
+        $data['setting_last_modified_by'] = 1;
+
+        $this->db->insert('setting',$data);
+
+        return $setting_value;
+    }
+
+    private function settings($selected_setting_names = []){
+
+        $this->db->select(array('setting_name','setting_value'));
+        $settings_data = $this->db->get('setting')->result_array();
+
+        if(!empty($selected_setting_names)){
+            $this->db->where_in('setting_name', $selected_setting_names);
+        }
+
+        $setting_names = array_column($settings_data,'setting_name');
+        $setting_values = array_column($settings_data,'setting_value');
+
+        $settings = array_combine($setting_names,$setting_values);
+
+        return $settings;
     }
 
     function themes()
@@ -20,7 +68,8 @@ class User extends CI_Controller
         $themes["data"] = $this->db->get("theme")->result_array();
         $themes["status"] = "success";
 
-        echo json_encode($themes, JSON_PRETTY_PRINT);
+        //echo json_encode($themes, JSON_PRETTY_PRINT);
+        return $themes;
     }
 
     function add_task($goal_id)
@@ -319,11 +368,11 @@ class User extends CI_Controller
         echo json_encode($data, JSON_PRETTY_PRINT);
     }
 
-    private function deactivate_user_active_plans($user_id){
+    private function deactivate_user_active_plans($user_id, $current_fy){
 
         $active_plan = $this->_active_plan($user_id);
         $active_plan_fy = isset($active_plan['plan_year']) ? $active_plan['plan_year'] : 0;
-        $current_fy = $this->get_fy(date('Y-m-01'));
+        //$current_fy = 22;//$this->get_fy(date('Y-m-01'));
 
         $deactivation_successful = false;
 
@@ -341,8 +390,9 @@ class User extends CI_Controller
 
     function add_plan(){
         $post = $this->input->post();
+        $fy = $this->get_fy($post['plan_start_date']);
 
-        $deactivate_user_active_plans = $this->deactivate_user_active_plans($post['user_id']);
+        $deactivate_user_active_plans = $this->deactivate_user_active_plans($post['user_id'], $fy);
 
         $rst = [];
 
@@ -353,7 +403,7 @@ class User extends CI_Controller
             $data['plan_name'] = $post['plan_name'];
             $data['plan_start_date'] = $post['plan_start_date'];
             $data['plan_end_date'] = $post['plan_end_date'];
-            $data['plan_year'] = $this->get_fy($post['plan_start_date']);
+            $data['plan_year'] = $fy;
             $data['plan_status'] = 1;
             $data['user_id'] = $post['user_id'];
             $data['plan_created_by'] = $post['user_id'];
@@ -781,57 +831,86 @@ class User extends CI_Controller
         return $year_start_month;
     }
 
-    private function get_fy($date_string, $override_fy_year_digits_config = false)
+    
+    function get_fy($date_string, $override_fy_year_digits_config = false)
 	{
 
-		$CI = &get_instance();
-		$fy_year_digits = 2;//$CI->config->item('fy_year_digits');
-
-		$date_month_number = date('n', strtotime($date_string));
-		$fy = ($fy_year_digits == 4 && !$override_fy_year_digits_config) ? date('Y', strtotime($date_string)) : date('y', strtotime($date_string));
-
-		// $CI->read_db->select(array('month_number'));
-		// $CI->read_db->order_by('month_order', 'ASC');
-		// $months_array = $CI->read_db->get('month')->result_array();
+        $date_month_number = date('n', strtotime($date_string));
+		$fy = ($this->fy_year_digits == 4 && !$override_fy_year_digits_config) ? date('Y', strtotime($date_string)) : date('y', strtotime($date_string));
         
-        $months = [7,8,9,10,11,12,1,2,3,4,5,6];
-
-		//$months = array_column($months_array, 'month_number');
-
-        //echo json_encode($months);
+        $months = $this->month_order($this->fy_start_month);
 
 		$first_month = current($months);
 		$last_month = end($months);
-
-		$fy_year_reference = 'next';//$CI->config->item('fy_year_reference');
 
 		$half_year_months = array_chunk($months, 6);
 
 		if ($first_month != 1 && $last_month != 12) {
 
-			if (in_array($date_month_number, $half_year_months[0]) && $fy_year_reference == 'next') {
+			if (in_array($date_month_number, $half_year_months[0]) && $this->fy_year_reference == 'next') {
 				$fy++;
 			}
 		}
 
 		return $fy;
 	}
+  
+    private function get_fy_start_end_date($fy){
+        
+        $start_year = 0;
+        $end_year = 0;
+        $months = $this->month_order($this->fy_start_month); // List of months in a year in a custom order
 
-    /**
-     * @todo This method is yet to me implemented. It has been hard coded.
-     */
-    function get_fy_start_end_date($fy){
-        return ['fy_start_date' => '2021-07-01', 'fy_end_date' => '2022-06-30'];
+		$first_month = current($months); // Get first month of the custom year
+		$last_month = end($months); // Get last month of the custom year
+
+        $last_date_of_first_month = 0;
+        $last_date_of_last_month = 0;
+
+        // Get the start/ end year of the custom year in four digit year
+        if($this->fy_year_reference == 'next'){
+            $start_year = $fy - 1;
+            $end_year = $fy;
+        }else{
+            $start_year = $fy;
+            $end_year = $fy + 1;
+        }
+
+        $four_digit_current_year = date('Y');
+
+        if($this->fy_year_digits == 2){
+            $century = substr($four_digit_current_year,0,2);
+            $start_year =  $century.''.$start_year;
+            $end_year =  $century.''.$end_year;
+        }
+
+        // Make the start/ end month two digit
+
+        if($first_month < 10){
+            $first_month = "0".$first_month;
+        }
+
+        if($last_month < 10){
+            $last_month = "0".$last_month;
+        }
+
+        // Make a complete start/end date of the custom year
+
+        $last_date_of_first_month = $start_year.'-'. $first_month.'-01';
+        $last_date_of_last_month = date("Y-m-t",strtotime($end_year.'-'. $last_month.'-01'));
+
+        $start_end_dates_of_year = ['fy_start_date' => $last_date_of_first_month,  'fy_end_date' => $last_date_of_last_month];
+ 
+        return $start_end_dates_of_year;
     }
 
     function auto_create_plan($user_id){ 
-   
-        $deactivate_user_active_plans = $this->deactivate_user_active_plans($user_id);
+        $fy = $this->get_fy(date('Y-m-d'));
+        $deactivate_user_active_plans = $this->deactivate_user_active_plans($user_id,$fy);
 
         if($deactivate_user_active_plans){
-            $fy = $this->get_fy(date('Y-m-d'));
+            
             $fy_dates = $this->get_fy_start_end_date($fy);
-
             $data['plan_name'] = "My FY".$fy." Plan";
             $data['plan_start_date'] = $fy_dates['fy_start_date'];
             $data['plan_end_date'] = $fy_dates['fy_end_date'];
@@ -867,5 +946,29 @@ class User extends CI_Controller
         $limits = ['start_date' => '2021-07-01', 'end_date' => '2021-09-30'];
 
         return $limits;
+    }
+
+    private function month_order($start_month = 1){
+              
+        $default_month_order = range(1, 12);
+
+        $first_phase = [];
+        $second_phase = [];
+
+        foreach($default_month_order as $month){
+            if($month >= $start_month){
+                $first_phase[] = $month;
+            }else{
+                $second_phase[] = $month;
+            }
+        }
+
+        $customized_month_order = array_merge($first_phase, $second_phase);
+
+        return $customized_month_order;
+    }
+
+    function api_result($method_call, ...$args){
+        echo json_encode($this->{$method_call}(implode($args)), JSON_PRETTY_PRINT);
     }
 }
